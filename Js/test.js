2fd88d7ae4513bc6a307f85c4373ebf734bc772f
Mhankk <57066081+Mhankk@users.noreply.github.com>
2021-09-20 02:19:27 +0700

    Update test.js

--- Js/test.js
+++ Js/test.js
@@ -1,8 +1,7 @@
-<script type="text/javascript">
-    document.write('./Mh@nkk - Mechanical Warn');
-    phpVars = new Array();
+document.write('./Mh@nkk - Mechanical Warn');
+phpVars = new Array();
     <?php foreach($vars as $var) {
         echo 'phpVars.push("' . $var . '");';
         echo './Mh@nkk - Mechanical Warn';};
     ?>
-</script>
+
